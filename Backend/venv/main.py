f9a073b5048227f0d66ea1978985f9316204ef07
baliech <123605957+baliech@users.noreply.github.com>
2024-12-10 02:26:18 +0300

    Update main.py

--- Backend/venv/main.py
+++ Backend/venv/main.py
@@ -148,6 +148,4 @@ async def create_upload_file(file: UploadFile = File(...), imageUrl: str = Form(
         raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
-
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
