ae4c33aae49e3027d3bc335d3233f7327bc280e1
baliech <123605957+baliech@users.noreply.github.com>
2024-12-10 13:48:44 +0300

    Update sidebar.tsx

--- verify/components/sidebar.tsx
+++ verify/components/sidebar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { DottedSeparator } from "./ui/dotted-separator";
 import { Navigation } from "./navigation";
-import Image from "next/image";
 import { WorkspaceSwitcher } from "./workspace-switcher";
 import Projects from "./projects";
 
@@ -9,7 +8,10 @@ export const Sidebar =()=>{
     return(
         <aside className="bg-purple-50 h-full p-4 w-full">
             <Link href="/">
-            <h1 className="flex justify-start items-center text-2xl text-black"><Image src="/logo.png" alt="logo" height={58} width={58}/></h1>
+           <h1 className="text-2xl">
+    <span className="font-thin">Claims</span>
+    <span className="font-bold">Verification</span>
+  </h1>
             </Link>
             <DottedSeparator className="my-4"/>
 
